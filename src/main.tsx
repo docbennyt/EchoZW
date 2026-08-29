8c31c790c67e8b303d2c58d68b2086fc29eb81a4
Dr BennyT <131795253+docbennyt@users.noreply.github.com>
2026-08-29 23:53:04 +0200

    style: load timetable discovery styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { AppV2 } from "./AppV2";
 import "./styles.css";
 import "./appV2.css";
+import "./finderDiscovery.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
